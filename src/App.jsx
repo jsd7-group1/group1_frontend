5dc4a959b9b058e7ea590839d961bf4ccddd9df9
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-06-17 01:57:20 +0700

    add import all product

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import { Link } from 'react-router-dom'
-import './App.css'
+import { Link } from "react-router-dom";
+import "./App.css";
+// import americanoPic from "./imgallpd/americano.jpg";
+import allProduct from "./imgallpd/index";
+console.log(allProduct);
 
 function App() {
-
   return (
-    <div className='flex flex-col justify-between gap-6 font-bold'>
-      <h1 className='text-center text-5xl'>Home Page for dev</h1>
-      <Link to="/home"><button>home</button></Link>
-      <Link to="/login"><button>login</button></Link>
-      <Link to="/register"><button>register</button></Link>
-      <Link to="/allProduct"><button>allProduct</button></Link>
-      <Link to="/cart"><button>cart</button></Link>
-      <Link to="/payment"><button>payment</button></Link>
+    <div className="flex flex-col justify-between gap-6 font-bold">
+      <h1 className="text-center text-5xl">Home Page for dev</h1>
+
+      <Link to="/home">
+        <button>home</button>
+      </Link>
+      <Link to="/login">
+        <button>login</button>
+      </Link>
+      <Link to="/register">
+        <button>register</button>
+      </Link>
+      <Link to="/allProduct">
+        <button>allProduct</button>
+      </Link>
+      <Link to="/cart">
+        <button>cart</button>
+      </Link>
+      <Link to="/payment">
+        <button>payment</button>
+      </Link>
+
+      {/* ###### ALL PICTURE ###### */}
+
+      <img src={allProduct.americano} width={120} height={120} />
+      <img src={allProduct.brewedcoffee} width={120} height={120} />
+      <img src={allProduct.cafemocha} width={120} height={120} />
+      <img src={allProduct.caramelmacchiato} width={120} height={120} />
+      <img src={allProduct.custardcream} width={120} height={120} />
+      <img src={allProduct.darkchoc} width={120} height={120} />
+      <img src={allProduct.expressoconpana} width={120} height={120} />
+      <img src={allProduct.iceclassicchoc} width={120} height={120} />
+      <img src={allProduct.iceflatwhite} width={120} height={120} />
+      <img src={allProduct.JumboSausage} width={120} height={120} />
+      <img src={allProduct.maltydelight} width={120} height={120} />
+      <img src={allProduct.mochafrap} width={120} height={120} />
+      <img src={allProduct.myrtillusCheeseCake} width={120} height={120} />
+      <img src={allProduct.prunus} width={120} height={120} />
+      <img src={allProduct.shot} width={120} height={120} />
+      <img src={allProduct.softoatmilk} width={120} height={120} />
+      <img src={allProduct.strawberrytart} width={120} height={120} />
+      <img src={allProduct.YuzuColdBrew} width={120} height={120} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/imgallpd/index.js
@@ -0,0 +1,41 @@
+import americanoPic from "./americano.jpg";
+import brewedcoffeePic from "./brewedcoffee.png";
+import cafemochaPic from "./cafemocha.png";
+import caramelmacchiatoPic from "./caramelmacchiato.png";
+import custardcreamPic from "./custardcream.png";
+import darkchocPic from "./darkchoc.jpg";
+import expressoconpanaPic from "./expressoconpana.png";
+import iceclassicchocPic from "./iceclassicchoc.jpg";
+import iceflatwhitePic from "./iceflatwhite.jpg";
+import JumboSausagePic from "./JumboSausage.png";
+import maltydelightPic from "./maltydelight.jpg";
+import mochafrapPic from "./mochafrap.png";
+import myrtillusCheeseCakePic from "./myrtillusCheeseCake.png";
+import prunusPic from "./prunus.jpg";
+import shotPic from "./shot.png";
+import softoatmilkPic from "./softoatmilk.png";
+import strawberrytartPic from "./strawberrytart.jpg";
+import YuzuColdBrewPic from "./YuzuColdBrew.png";
+
+const allProduct = {
+  americano: americanoPic,
+  brewedcoffee: brewedcoffeePic,
+  cafemocha: cafemochaPic,
+  caramelmacchiato: caramelmacchiatoPic,
+  custardcream: custardcreamPic,
+  darkchoc: darkchocPic,
+  expressoconpana: expressoconpanaPic,
+  iceclassicchoc: iceclassicchocPic,
+  iceflatwhite: iceflatwhitePic,
+  JumboSausage: JumboSausagePic,
+  maltydelight: maltydelightPic,
+  mochafrap: mochafrapPic,
+  myrtillusCheeseCake: myrtillusCheeseCakePic,
+  prunus: prunusPic,
+  shot: shotPic,
+  softoatmilk: softoatmilkPic,
+  strawberrytart: strawberrytartPic,
+  YuzuColdBrew: YuzuColdBrewPic,
+};
+
+export default allProduct;
