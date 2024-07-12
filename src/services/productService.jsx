de6ce90071a690a67e9f4d35c6e48635c908407a
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-07-12 14:52:21 +0700

    All product API

---
+++ src/services/productService.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8081/products";
+
+const axiosInstance = axios.create({
+    baseURL,
+})
+
+const fetchProduct = async () => {
+    try {
+        const response = await axiosInstance.get("/");
+        return response.data;
+    } catch (error) {
+        console.log("Error fetching product",error);
+        throw error
+    }
+};
+
+const productByCategory = async () =>{
+    try {
+        const response = await axiosInstance.get(`/${categoryID}`);
+        return response.data;
+    } catch (error) {
+        console.log("Error fetching product",error);
+        throw error
+    }
+}
+export { fetchProduct, productByCategory }
